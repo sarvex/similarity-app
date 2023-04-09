7b644fb219d0b3f7b3789e94ef0620352bdc9233
Sarvex <sarvex@jatasra.com>
2023-04-09 13:48:15 +0530

    Add UI Components

--- app/components/ui/Paragraph.tsx
+++ app/components/ui/Paragraph.tsx
@@ -1,11 +1,39 @@
-import { FC } from 'react'
+import { HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
-interface ParagraphProps {}
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm, sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-  return (
-    <div>Paragraph</div>
-  )
-}
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
 
-export default Paragraph
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p 
+      ref={ref}
+        {...props}
+        className={cn(paragraphVariants({ size, className }))}>
+        {children}
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = "Paragraph";
+
+export default Paragraph;
