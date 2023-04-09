15f69a9b9cb83262576d52a17ecec66977093427
Sarvex <sarvex@jatasra.com>
2023-04-09 11:53:06 +0530

    Initial Version

---
+++ app/components/ui/Paragraph.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ParagraphProps {}
+
+const Paragraph: FC<ParagraphProps> = ({}) => {
+  return (
+    <div>Paragraph</div>
+  )
+}
+
+export default Paragraph
